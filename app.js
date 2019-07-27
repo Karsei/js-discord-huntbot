b65b694afb465a56c1c4f5f73c29b3b7f5b2a524
Karsei <karseidev@gmail.com>
2019-07-27 14:36:17 +0900

    * 서버 업타임 커맨드 추가

--- app.js
+++ app.js
@@ -275,6 +275,9 @@ class MobHuntDiscord {
 
         let _this = oMobHuntDiscord;
 
+        // 행위 추가
+        client.user.setPresence({ game: { name: 'FFXIV: SHB' }, status: 'online' });
+
         // 메세지 풀 초기화
         _this.oMsgPool = new MessagePool();
 
@@ -433,6 +436,29 @@ class MobHuntDiscord {
                     pGuild.channels.forEach((pChannel) => { _clStr += ` -- ${pChannel.name} (${pChannel.type}) - ${pChannel.id}\n`; });
                 });
                 pObjMsg.channel.send(_clStr);
+                break;
+            // 업타임
+            case ';!UPTIME':
+                let _sec = 1000;
+                let _min = _sec * 60;
+                let _hour = _min * 60;
+                let _day = _hour * 24;
+
+                let diffDate = new Date() - oStartDate;
+                let d_day = Math.floor(diffDate / _day);
+                let d_hour = Math.floor((diffDate % _day) / _hour);
+                let d_min = Math.floor((diffDate % _hour) / _min);
+                let d_sec = Math.floor((diffDate % _min) / _sec);
+                let d_msec = diffDate % _sec;
+                
+                let diffDateStr = '';
+                if (d_day > 0)  diffDateStr += d_day + '일 ';
+                if (d_hour > 0) diffDateStr += d_hour + '시간 ';
+                if (d_min > 0)  diffDateStr += d_min + '분 ';
+                if (d_sec > 0)  diffDateStr += d_sec + '초 ';
+                if (d_msec > 0)  diffDateStr += d_msec + '';
+
+                pObjMsg.channel.send(`서버 업타임: ${diffDateStr}`);
                 break;
         }
     }
@@ -704,6 +730,9 @@ const FFXIV_ALL_SERVERS = {
 
 // 디스코드 마물 처리
 const oMobHuntDiscord = new MobHuntDiscord();
+
+// 프로그램 시작시간
+const oStartDate = new Date();
 
 /************************************************
  * VARIABLES
